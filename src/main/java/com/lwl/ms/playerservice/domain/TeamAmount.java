01c5fe1fc7379c5f4a33b4c352a26e11ca868945
M Rajesh <m.rajeshrajz@gmail.com>
2021-07-24 23:03:03 +0530

    added getter and setter

--- src/main/java/com/lwl/ms/playerservice/domain/TeamAmount.java
+++ src/main/java/com/lwl/ms/playerservice/domain/TeamAmount.java
@@ -1,5 +1,12 @@
 package com.lwl.ms.playerservice.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
 public class TeamAmount {
 
 	private double amount;
