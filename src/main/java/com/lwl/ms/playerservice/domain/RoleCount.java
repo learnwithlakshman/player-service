58cffa5b67973b489ae74608cbf7ca02c6b5f662
M Rajesh <m.rajeshrajz@gmail.com>
2021-07-24 20:03:16 +0530

    added Builder

--- src/main/java/com/lwl/ms/playerservice/domain/RoleCount.java
+++ src/main/java/com/lwl/ms/playerservice/domain/RoleCount.java
@@ -1,5 +1,12 @@
 package com.lwl.ms.playerservice.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
 public class RoleCount {
 
 	private String role;
